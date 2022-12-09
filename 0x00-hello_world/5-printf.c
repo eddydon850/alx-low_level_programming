db601158860fc6467d0d3a6b96a2a4ccd74149a4
eddydon850 <obiozoredward@gmail.com>
2022-12-08 16:09:19 -0800

    Task 5 printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-        printf("with proper grammar,but the outcome is a piece art,\n");
-	return (0);
+printf("with proper grammar,but the outcome is a piece art,\n");
+return (0);
 
 }
